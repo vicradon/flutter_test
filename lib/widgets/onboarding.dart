6073d3c62ce77bd5080ad05e33e091992ff5dd0b
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-05-10 07:50:04 +0100

    feat: onboarding 90%

--- lib/widgets/onboarding.dart
+++ lib/widgets/onboarding.dart
@@ -1,40 +1,137 @@
-import 'package:introduction_screen/introduction_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
-final listPagesViewModel = [
-  PageViewModel(
-    title: "Title of first page",
-    body:
-        "Here you can write the description of the page, to explain someting...",
-    image: Center(
-      child: Image.asset("assets/images/screen1.png", height: 175.0),
-    ),
-  ),
-  PageViewModel(
-    title: "Title of first page",
-    body:
-        "Here you can write the description of the page, to explain someting...",
-    image: Center(child: Icon(Icons.android)),
-    decoration: PageDecoration(
-      titleTextStyle: TextStyle(color: Colors.orange),
-      bodyTextStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 20.0),
-    ),
-  )
+class Walkthrougth extends StatelessWidget {
+  final String heading;
+  final String textContent;
+  final String image;
+
+  Walkthrougth(
+      {Key key,
+      @required this.heading,
+      @required this.textContent,
+      @required this.image})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Color(0xFFF9F9F9)),
+      width: MediaQuery.of(context).size.width,
+      // height: MediaQuery.of(context).size.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(image),
+          Text(
+            heading,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text(textContent),
+        ],
+      ),
+    );
+  }
+}
+
+class Dots extends StatelessWidget {
+  final double currentIndexPage;
+  final int pageLength;
+  Dots({this.currentIndexPage, this.pageLength});
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: MediaQuery.of(context).size.height * 0.94,
+      child: Padding(
+        padding:
+            EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.05),
+        child: DotsIndicator(
+          dotsCount: pageLength,
+          position: currentIndexPage,
+          decorator: DotsDecorator(
+            activeColor: Color(0xFFF8A700),
+            size: Size.square(9.0),
+            activeSize: Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SkipButton extends StatelessWidget {
+  final double currentIndexPage;
+  SkipButton(this.currentIndexPage);
+
+  @override
+  Widget build(BuildContext context) {
+    String text = "skip";
+
+    if (currentIndexPage == 3) {
+      text = "done";
+    }
+    return Positioned(
+      top: MediaQuery.of(context).size.height * 0.92,
+      left: MediaQuery.of(context).size.width * 0.75,
+      child: FlatButton(
+        child: Text(text),
+        onPressed: () {
+          print("skip");
+        },
+      ),
+    );
+  }
+}
+
+class Onboarding extends StatefulWidget {
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+List onboardingScreens = <Widget>[
+  Walkthrougth(
+      heading: "Excellence",
+      textContent: "Be excellent in your own way",
+      image: "assets/images/screen1.png"),
+  Walkthrougth(
+      heading: "Search for solutions",
+      textContent: "Search for solutions with lightspeed",
+      image: "assets/images/screen2.png"),
+  Walkthrougth(
+      heading: "Tips",
+      textContent: "Gain helpful tips as you progress",
+      image: "assets/images/screen3.png")
 ];
 
-class Onboarding extends StatelessWidget {
+class _OnboardingState extends State<Onboarding> {
+  double currentIndexPage;
+  int pageLength;
+
+  @override
+  void initState() {
+    currentIndexPage = 0;
+    pageLength = 3;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IntroductionScreen(
-        pages: listPagesViewModel,
-        onDone: () {
-          // When done button is press
-        },
-        showSkipButton: true,
-        skip: Text("Skip"),
-        next: Icon(Icons.chevron_right),
-        done: Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+      body: Stack(
+        children: <Widget>[
+          PageView(
+            children: onboardingScreens,
+            onPageChanged: (value) {
+              setState(() {
+                currentIndexPage = value + .0;
+              });
+            },
+          ),
+          Dots(currentIndexPage: currentIndexPage, pageLength: pageLength),
+          SkipButton(currentIndexPage)
+        ],
       ),
     );
   }
